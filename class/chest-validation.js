760fa0271f84dba4ffb841de8dba6eaeb0dd7d64
Hoishin <hoishinxii@gmail.com>
2017-08-28 02:00:18 +0900

    chest-validation.js (WIP)

---
+++ class/chest-validation.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = class ChestValidation {
+	constructor(
+		appear = 50,
+		position = 1,
+		gilChance = 50,
+		itemChance = 50,
+		gilAmount = 100
+	) {
+		this.appear = appear;
+		this.position = position;
+		this.gilChance = gilChance;
+		this.itemChance = itemChance;
+		this.gilAmount = gilAmount;
+	}
+	content() {
+		
+	}
+}
