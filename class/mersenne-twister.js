a6ce5371ca144f7a92ea2d76ad08e7f231203157
Hoishin <hoishinxii@gmail.com>
2017-08-28 01:59:41 +0900

    mersenne-twiester.js

---
+++ class/mersenne-twister.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const DEFAULT_SEED = 4537;
+const N = 624;
+const M = 397;
+const MATRIX_A = 0x9908B0DF;
+const UPPER_MASK = 0x80000000;
+const LOWER_MASK = 0x7FFFFFFF;
+
+module.exports = class MersenneTwister {
+	constructor(tza = false) {
+		this.tza = tza;
+		this.mt = [N];
+		this.mti = N + 1;
+		this.initGenRand(DEFAULT_SEED);
+	}
+	initGenRand(seed) {
+		this.mt[0] = seed >>> 0;
+		for (this.mti = 1; this.mti < N; this.mti++) {
+			if (this.tza) {
+				const s = this.mt[this.mti - 1] ^ (this.mt[this.mti - 1] >>> 30);
+				const first = (((s & 0xFFFF0000) >>> 16) * 1812433253) << 16;
+				const second = (s & 0x0000FFFF) * 1812433253;
+				this.mt[this.mti] = (first + second) + this.mti;
+				this.mt[this.mti] >>>= 0;
+			} else {
+				this.mt[this.mti] = (69069 * this.mt[this.mti - 1]) & 0xFFFFFFFF;
+			}
+		}
+	}
+	genRand() {
+		let y;
+		const mag01 = [0x0, MATRIX_A];
+		if (this.mti >= N) {
+			let kk;
+			if (this.mti === N + 1) {
+				this.initGenRand(DEFAULT_SEED);
+			}
+			for (kk = 0; kk < N - M; kk++) {
+				y = (this.mt[kk] & UPPER_MASK) | (this.mt[kk + 1] & LOWER_MASK);
+				this.mt[kk] = this.mt[kk + M] ^ (y >>> 1) ^ mag01[y & 0x1];
+			}
+			for (; kk < N - 1; kk++) {
+				y = (this.mt[kk] & UPPER_MASK) | (this.mt[kk + 1] & LOWER_MASK);
+				this.mt[kk] = this.mt[kk + (M - N)] ^ (y >>> 1) ^ mag01[y & 0x1];
+			}
+			y = (this.mt[N - 1] & UPPER_MASK) | (this.mt[0] & LOWER_MASK);
+			this.mt[N - 1] = this.mt[M - 1] ^ (y >>> 1) ^ mag01[y & 0x1];
+			this.mti = 0;
+		}
+		y = this.mt[this.mti++];
+		y ^= (y >>> 11);
+		y ^= (y << 7) & 0x9D2C5680;
+		y ^= (y << 15) & 0xEFC60000;
+		y ^= (y >>> 18);
+		return y >>> 0;
+	}
+};
