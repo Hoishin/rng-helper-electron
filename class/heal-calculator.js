ea4cc97b55670dfe5798fb47b8fea8f299d22b66
Hoishin <hoishinxii@gmail.com>
2017-08-29 20:59:32 +0900

    Working Logic of RNG finding

--- class/heal-calculator.js
+++ class/heal-calculator.js
@@ -14,14 +14,16 @@ module.exports = class HealCalculator {
 		this.magick = magick;
 		this.serenity = serenity ? 1.5 : 1;
 	}
-	calculate(random = 1288459236) {
+	calculate(random = 0) {
 		const result = (this.spell + (random % (this.spell * 12.5) / 100)) *
 			(((this.level + this.magick) * (this.magick / 256)) + 2) * this.serenity;
 		return Math.floor(result);
 	}
 	validNumber(heal) {
-		const minValue = this.calculate(0);
-		const maxValue = this.calculate(2147483647);
+		const minValue = (this.spell + (0 / 100)) *
+		(((this.level + this.magick) * (this.magick / 256)) + 2) * this.serenity;
+		const maxValue = (this.spell + ((this.spell * 12.5) - 1 / 100)) *
+		(((this.level + this.magick) * (this.magick / 256)) + 2) * this.serenity;
 		if (heal < minValue || heal > maxValue) {
 			return false;
 		}
--- test.js
+++ test.js
@@ -1,160 +1,102 @@
+'use strict';
+
 const MersenneTwiester = require('./class/mersenne-twister');
 const HealCalculator = require('./class/heal-calculator');
 
 const rngPs2 = new MersenneTwiester(false);
 const calcCure = new HealCalculator('cure', 3, 23, false);
+const DEFAULT_LENGTH = 10000; // Number of RNG you want to hold in memory
+let position = 0; // Tracks where RNG is at
+let healNumbers = []; // Holds heal numbers in reverse order
+let past = []; // Holds RNGs right before the current RNG
+let future = []; // Holds next DEFAULT_LENGTH of RNG
 
-const DEFAULT_LENGTH = 10;
-
-let cureNumbers = [];
-let past = [];
-let future = [];
-
-// Make initial future table
+// Make initial future array
 for (let i = 0; i < DEFAULT_LENGTH; i++) {
-	const random = rngPs2.genRand();
-	future.push({
-		position: i,
-		heal: calcCure.calculate(random),
-		percent: (random % 100) + 1
-	});
+	generateRandom(rngPs2.genRand());
 }
-console.log('default:');
-console.log(future);
 
+// Input heal numbers here
+findPosition(90);
 findPosition(91);
-findPosition(88);
-findPosition(86);
+findPosition(91);
+findPosition(89);
+findPosition(80);
+findPosition(92);
+findPosition(92);
+findPosition(92);
+findPosition(92);
+findPosition(92);
 
+// Output position found
+console.log(past);
+
+// Main function that look for the position
 function findPosition(input) {
-	// Validate heal amount and reset if it's false
+	// Validate heal amount and ignore if it's invalid
 	if (!calcCure.validNumber(input)) {
-		console.log('Invalid number');
-		cureNumbers = [];
+		console.log('Encountered Invalid Number: ' + input);
 		return;
 	}
-	// Reverse order for convinience
-	cureNumbers.unshift(input);
-	// If input number is next number, just move 1 up
-	if (cureNumbers[0] === future[0].heal) {
+
+	// Store heal numbers in reverse order because it's actually easier to handle
+	healNumbers.unshift(input);
+
+	// If heal number matches the next number, just move once
+	if (healNumbers[0] === future[0].heal) {
 		past.push(future.shift());
-		const random = rngPs2.genRand();
-		future.push({
-			position: future[future.length - 1].position + 1,
-			heal: calcCure.calculate(random),
-			percent: (random % 100) + 1
-		});
-		console.log('>>>>>>>>found it<<<<<<<<');
-		console.log('Past:');
-		console.log(past);
-		console.log('Future:');
-		console.log(future);
+		generateRandom(rngPs2.genRand());
 	} else {
+		// Loop until it finds the position
 		let found = false;
-		// Try to find cure number in the existing array.
-		for (let j = 1; j < future.length; j++) {
-			if (cureNumbers[0] === future[j].heal) {
-				// Found the number in existing array at j
-				// Checks if sequense matches
+		while (!found) {
+			// Look for the number in the future array
+			for (let i = 0; i < future.length; i++) {
 				let sequenceMatch = true;
-				for (let i = 1; i < cureNumbers.length && j - i >= 0; i++) {
-					if (cureNumbers[i] !== future[j - i].heal) {
+				// Check if the sequence matches
+				for (let j = 0; j < healNumbers.length; j++) {
+					if (i - j < 0 || healNumbers[j] !== future[i - j].heal) {
+						// Sequence doesn't match
 						sequenceMatch = false;
 						break;
 					}
 				}
+				
+				// Sequence matches
 				if (sequenceMatch) {
-					// Then move the items in future to past, and add more items
-					for (let i = 0; i < j + 1; i++) {
-						past.push(future.shift());
-						if (past.length > cureNumbers.length) {
-							past.shift();
-						}
-						const random = rngPs2.genRand();
-						future.push({
-							position: future[future.length - 1].position + 1,
-							heal: calcCure.calculate(random),
-							percent: (random % 100) + 1
-						});
+					// Store RNGs up to the RNG it found
+					past = future.splice(0, i + 1).splice(-healNumbers.length, healNumbers.length);
+					// Generate RNGs to make up for the RNG spent to find the RNG
+					for (let j = 0; j < i + 1; j++) {
+						generateRandom(rngPs2.genRand());
+					}
+					// Cut off past array if it has more than how many heal numbers are
+					if (past.length > healNumbers.length) {
+						past = past.splice(-healNumbers.length, healNumbers.length);
 					}
 					found = true;
-					console.log('>>>>>>>>found it<<<<<<<<');
-					console.log('Past:');
-					console.log(past);
-					console.log('Future:');
-					console.log(future);
 					break;
 				}
 			}
-		}
 
-		// Couldn't find the number in the existing array
-		while (!found) {
-			const position = future[future.length - 1].position + 1;
-			past = [];
-			future = future.splice(-cureNumbers.length + 1, cureNumbers.length - 1);
-			// Make initial table
-			for (let i = 0; future.length <= DEFAULT_LENGTH; i++) {
-				const random = rngPs2.genRand();
-				future.push({
-					position: position + i,
-					heal: calcCure.calculate(random),
-					percent: (random % 100) + 1
-				});
-			}
-			// If input number is next number, just move 1 up
-			if (cureNumbers[0] === future[0].heal) {
-				past.push(future.shift());
-				const random = rngPs2.genRand();
-				future.push({
-					position: future[future.length - 1].position + 1,
-					heal: calcCure.calculate(random),
-					percent: (random % 100) + 1
-				});
-				found = true;
-				console.log('>>>>>>>>found it<<<<<<<<');
-				console.log('Past:');
-				console.log(past);
-				console.log('Future:');
-				console.log(future);
-			} else {
-				// Try to find cure number in the existing array.
-				for (let j = 1; j < future.length; j++) {
-					if (cureNumbers[0] === future[j].heal) {
-						// Found the number in existing array at j
-						// Checks if sequense matches
-						let sequenceMatch = true;
-						for (let i = 1; i < cureNumbers.length; i++) {
-							if (j - i >= 0 || cureNumbers[i] !== future[j - i].heal) {
-								sequenceMatch = false;
-								break;
-							}
-						}
-						if (sequenceMatch) {
-							// Then move the items in future to past, and add more items
-							for (let i = 0; i < j + 1; i++) {
-								past.push(future.shift());
-								if (past.length > cureNumbers.length) {
-									past.shift();
-								}
-								const random = rngPs2.genRand();
-								future.push({
-									position: future[future.length - 1].position + 1,
-									heal: calcCure.calculate(random),
-									percent: (random % 100) + 1
-								});
-							}
-							found = true;
-							console.log('>>>>>>>>found it<<<<<<<<');
-							console.log('Past:');
-							console.log(past);
-							console.log('Future:');
-							console.log(future);
-							break;
-						}
-					}
+			// Couldn't find RNG in current future array
+			// Refresh and load next future array and continue the loop
+			if (!found) {
+				past = [];
+				future.splice(0, future.length - healNumbers.length);
+				for (let i = 0; i < DEFAULT_LENGTH; i++) {
+					generateRandom(rngPs2.genRand());
 				}
 			}
 		}
 	}
+}
+
+// Takes random number from Mersenne Twister, adds RNG to future array
+function generateRandom(random) {
+	future.push({
+		position: position++,
+		heal: calcCure.calculate(random),
+		percent: (random % 100)
+	})
 }
