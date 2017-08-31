97a734a7dcc649d9a4bce0a43706b9c9f6bb52b3
Hoishin <hoishinxii@gmail.com>
2017-08-31 23:15:13 +0900

    Chest Validation (working and pretty fast)

--- class-test.js
+++ class-test.js
@@ -1,15 +1,56 @@
 const FindPosition = require('./class/find-position');
+const ChestValidation = require('./class/chest-validation');
 
-const positionFinder = new FindPosition({
-	tza: false,
-	heal: {
-		spell: 'cure',
-		level: 3,
-		magick: 23,
-		serenity: false
-	},
-	defaultLength: 100000
-});
+const chest = new ChestValidation();
+const output = ['position', 'heal', 'appears', 'content'];
+const futureAmount = 10;
+
+class ChestFinder extends FindPosition {
+	calcChestContent() {
+		for (let i = 0; i < this.future.length - 1; i++) {
+			if (this.future[i].content === 'gil') {
+				this.future[i].content = chest.gilAmount(this.future[i + 1].random) + ' gil';
+			} else {
+				this.future[i].content = chest.itemContent(this.future[i + 1].percent);
+			}
+		}
+	}
+
+	__futurePush(random) {
+		const percent = random % 100;
+		this.future.push({
+			position: this.position++,
+			heal: this.heal.calculate(random),
+			percent,
+			appears: chest.ifAppears(percent),
+			content: chest.isGil(percent) ? 'gil' : 'item',
+			random
+		});
+	}
+
+	returnPast() {
+		return this.past.map(currentValue => {
+			const result = [];
+			output.forEach(element => {
+				result.push(currentValue[element]);
+			}, this);
+			return result;
+		});
+	}
+
+	returnFuture() {
+		const result = [];
+		for (let i = 0; i < futureAmount; i++) {
+			const resultItem = [];
+			output.forEach(element => {
+				resultItem.push(this.future[i][element]);
+			}, this);
+			result.push(resultItem);
+		}
+		return result;
+	}
+}
+const positionFinder = new ChestFinder();
 
 const startTime = Date.now();
 positionFinder.find(90);
@@ -17,6 +58,7 @@ positionFinder.find(90);
 positionFinder.find(90);
 positionFinder.find(90);
 positionFinder.find(90);
-positionFinder.find(90);
-console.log(positionFinder.past);
+positionFinder.calcChestContent();
+console.log(positionFinder.returnPast());
+console.log(positionFinder.returnFuture());
 console.log(Date.now() - startTime);
--- class/chest-validation.js
+++ class/chest-validation.js
@@ -1,26 +1,66 @@
 'use strict';
 
+const defaultChestConfig = {
+	appearChance: 50,
+	position: 1,
+	gilChance: 50,
+	item1Chance: 50,
+	maxGil: 100,
+	fixedGil: false
+};
+
 module.exports = class ChestValidation {
-	constructor(
-		appear = 50,
-		position = 1,
-		gilChance = 50,
-		itemChance = 50,
-		gilAmount = 100
-	) {
-		this.appear = appear;
-		this.position = position;
-		this.gilChance = gilChance;
-		this.itemChance = itemChance;
-		this.gilAmount = gilAmount;
+	/**
+	 * Sets up the chest and take care of it
+	 * @param {Object} chestConfig
+	 */
+	constructor(chestConfig = defaultChestConfig) {
+		this.config = chestConfig;
 	}
 
-	appear(chance) {
-		if (chance < this.appear) {
+	/**
+	 * See if the chest appears with the RNG
+	 * @param {Number} percent
+	 * @return {Boolean}
+	 */
+	ifAppears(percent) {
+		if (percent < this.config.appearChance) {
 			return true;
 		}
 		return false;
 	}
 
-	content()
-}
+	isGil(percent) {
+		if (percent < this.config.gilChance) {
+			return true;
+		}
+		return false;
+	}
+
+	gilAmount(random) {
+		if (this.config.fixedGil) {
+			return this.config.maxGil;
+		}
+		return 1 + (random % this.config.maxGil);
+	}
+
+	itemContent(percent) {
+		if (percent < this.config.item1Chance) {
+			return 'item1';
+		}
+		return 'item2';
+	}
+
+	/**
+	 * Determines next RNG position that spawns the chest
+	 * @param {Object[]} future
+	 * @return {Number}
+	 */
+	nextAppear(future) {
+		future.forEach((element, index) => {
+			if (this.ifAppears(element.percent)) {
+				return index;
+			}
+		}, this);
+	}
+};
